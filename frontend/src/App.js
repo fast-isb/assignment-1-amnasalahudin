1e4052b5dbe72203573be998425d112d038f33d3
amna0488 <amnasalahudin123@gmail.com>
2022-11-13 17:29:34 +0500

    backend test branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,29 +18,29 @@ function App() {
       <Router>
       <Switch>
 
-    // if logged in then homepage, otherwise display the login page
+   
       <Route exact path="/">
           {
             user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
           }
       </Route>
 
-          // for log in
+          
       <Route path="/login">
            <Login setLoginUser={setLoginUser}/>
       </Route>
 
-          // for sign up page
+        
       <Route path="/register">
       <Signup />
       </Route>
 
-          // for create product page
+          
       <Route path="/createProduct">
       <AddProduct />
       </Route>
 
-      // for view product page
+    
       <Route path="/viewProduct">
       <ViewProduct />
       </Route>
